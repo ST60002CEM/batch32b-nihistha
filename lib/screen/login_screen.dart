3f9f945e580cdccaa5b7d1fe06bc185499a9e4cb
nihistha <nihirastha20@gmail.com>
2024-05-15 10:17:56 +0545

    login screen

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("Welcome!",
+            style: TextStyle(
+              color: Color.fromRGBO(0x94, 0x6E, 0x54, 1.0),
+              fontSize: 55,
+            ),
+            ),
+            Text("Create a new account",
+            style: TextStyle(
+              color: Color.fromRGBO(0x94, 0x6E, 0x54, 1.0),
+              fontSize: 20,
+            ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Full Name",
+              ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Username",
+              ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Email",
+              ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                labelText: "Phone Number",
+              ),
+            ),
+            TextField(
+              decoration: const InputDecoration(
+                  labelText: "PassWord"
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 10,
+              child: ElevatedButton(onPressed: () {}, child: Text("Sign Up"),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromRGBO(0xD9, 0xA2, 0x6C, 1.0),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)
+                  ),
+
+                ),
+              ),
+            )
+          ],
+
+        ),
+      ),
+    );
+  }
+}
