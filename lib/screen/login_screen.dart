0a8f809647b059ef9d690b92f810503aea8136d9
nihistha <nihirastha20@gmail.com>
2024-05-16 13:31:07 +0545

    login screen done

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,12 +1,136 @@
+import 'package:adoptapet/screen/register_screen.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFFEFAE0),
+      appBar: AppBar(
+        leading: Image.asset('assets/images/logo.png'),
+        title: RichText(
+          text: TextSpan(
+            children: [
+              TextSpan(
+                text: 'Adopt',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.black,
+                ),
+              ),
+              TextSpan(
+                text: 'apet',
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.orange,
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Color(0xFFFEFAE0),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Welcome!",
+            style: TextStyle(
+              fontSize: 40,
+              color: Color(0xFF946E54),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Text("Login to continue",
+            style: TextStyle(
+              fontSize: 20,
+              color: Color(0xFF946E54),
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(30),
+              child:Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Username"
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          hintText: "Password"
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 100,
+                        child: ElevatedButton(onPressed: (){},
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Color(0xFFD9A26C), // Background color
+                            ),
+                            child: Text("Login",
+                              style: TextStyle(
+                                color: Color(0xFFFEFAE0),
+                                fontSize: 20
+                              ),
+                            )
+                        )
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?",
+                          style: TextStyle(
+                            fontSize: 18
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            // Navigate to sign-up screen
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (_) => RegisterScreen()),
+                            );
+                          },
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                              color: Color(0xFFD9A26C),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
 
+                ),
+            ),
+          ),
+
+        ],
+      ),
     );
   }
 }
+
--- lib/screen/register_screen.dart
+++ lib/screen/register_screen.dart
@@ -6,66 +6,10 @@ class RegisterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30),
-        child: Stack(
-          children: [
-            Text("Welcome!",
-              style: TextStyle(
-                color: Color.fromRGBO(0x94, 0x6E, 0x54, 1.0),
-                fontSize: 55,
-              ),
-            ),
-            Text("Create a new account",
-              style: TextStyle(
-                color: Color.fromRGBO(0x94, 0x6E, 0x54, 1.0),
-                fontSize: 20,
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Full Name",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Username",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Phone Number",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                  labelText: "PassWord"
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 10,
-              child: ElevatedButton(onPressed: () {}, child: Text("Sign Up"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromRGBO(0xD9, 0xA2, 0x6C, 1.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                  ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
 
-                ),
-              ),
-            )
-
-          ],
-
-        ),
       ),
     );
   }
