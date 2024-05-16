1806d94c11e620b8fcd45d19017d2ce948fd1a3a
nihistha <nihirastha20@gmail.com>
2024-05-16 12:43:54 +0545

    splash screen done

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,13 +1,15 @@
 import 'package:adoptapet/screen/login_screen.dart';
 import 'package:flutter/material.dart';
 
+import '../screen/splash_screen.dart';
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: LoginScreen(),
+      home: SplashScreen(),
     );
   }
 }
--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -6,68 +6,7 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text("Welcome!",
-            style: TextStyle(
-              color: Color.fromRGBO(0x94, 0x6E, 0x54, 1.0),
-              fontSize: 55,
-            ),
-            ),
-            Text("Create a new account",
-            style: TextStyle(
-              color: Color.fromRGBO(0x94, 0x6E, 0x54, 1.0),
-              fontSize: 20,
-            ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Full Name",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Username",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Email",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                labelText: "Phone Number",
-              ),
-            ),
-            TextField(
-              decoration: const InputDecoration(
-                  labelText: "PassWord"
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 10,
-              child: ElevatedButton(onPressed: () {}, child: Text("Sign Up"),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color.fromRGBO(0xD9, 0xA2, 0x6C, 1.0),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10)
-                  ),
 
-                ),
-              ),
-            )
-          ],
-
-        ),
-      ),
     );
   }
 }
