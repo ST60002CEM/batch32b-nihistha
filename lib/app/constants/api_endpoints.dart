23a468d5072aa757ccb179dffcd28a5e78f38a8e
nihistha <nihirastha20@gmail.com>
2024-06-20 13:55:55 +0545

    login register done

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -3,11 +3,12 @@ class ApiEndpoints{
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://10.0.2.2:3000/api/v1/";
+  static const String baseUrl = "http://10.1.6.59:5500/api/";
   //static const String baseUrl = "http://localhost:3000/api/v1/";
   //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
 
   // ====================== Auth Routes ======================
-  static const String login = "auth/login";
-  static const String register = "auth/register";
+  static const String login = "user/login";
+  static const String register = "user/create";
+  static const String currentUser = "user/getMe";
 }
--- lib/feature/auth/data/data_source/remote/auth_remote_data_source.dart
+++ lib/feature/auth/data/data_source/remote/auth_remote_data_source.dart
@@ -7,6 +7,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../../../core/error/failure.dart';
 import '../../../../../core/networking/remote/http_service.dart';
 import '../../../../../core/shared_prefs/user_shared_prefs.dart';
+import '../../dto/get_current_user_dto.dart';
 
 final authRemoteDataSourceProvider = Provider(
       (ref) => AuthRemoteDataSource(
@@ -68,6 +69,44 @@ class AuthRemoteDataSource{
         // Save token to shared prefs
         await userSharedPrefs.setUserToken(token);
         return const Right(true);
+      } else {
+        return Left(
+          Failure(
+            error: response.data["message"],
+            statusCode: response.statusCode.toString(),
+          ),
+        );
+      }
+    } on DioException catch (e) {
+      return Left(
+        Failure(
+          error: e.error.toString(),
+          statusCode: e.response?.statusCode.toString() ?? '0',
+        ),
+      );
+    }
+  }
+  Future<Either<Failure, AuthEntity>> getCurrentUser() async {
+    try {
+      // Get the token from shared prefs
+      String? token;
+      var data = await userSharedPrefs.getUserToken();
+      data.fold(
+            (l) => token = null,
+            (r) => token = r!,
+      );
+
+      var response = await dio.get(
+        ApiEndpoints.currentUser,
+        options: Options(headers: {
+          'Authorization': 'Bearer $token',
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        GetCurrentUserDto getCurrentUserDto = GetCurrentUserDto.fromJson(response.data);
+
+        return Right(getCurrentUserDto.toEntity());
       } else {
         return Left(
           Failure(
